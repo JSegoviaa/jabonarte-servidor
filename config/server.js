2f0e082470143d51e094a5034e6469ca0d08ea41
JSegovia <acosta.segoviaa@hotmail.com>
2021-05-23 19:12:31 -0500

    Se cambió favicon

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a4f27b65d6922070e68b853d695fef28'),
+      secret: env("ADMIN_JWT_SECRET", "a4f27b65d6922070e68b853d695fef28"),
     },
   },
 });
